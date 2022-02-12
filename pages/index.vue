3696c3edf7f80896460f773d10abb81f588b582a
klayafterlife <klayafterlife@gmail.com>
2022-02-12 16:43:06 +0900

    프로젝트 소개 수정

--- pages/index.vue
+++ pages/index.vue
@@ -3,31 +3,37 @@
     <nav-header />
 
     <div class="container">
-      <h1 class="mb-5">Text Kepler</h1>
+      <h1 class="mb-5 mt-5">Text Kepler</h1>
 
-      <intro v-show="tab === 0" @connected="tab = 1" />
+      <div class="mb-3">
+        이 프로젝트는 진화와 증식이 가능한 메타세포(Evol NFT)<br />
+        <a class="link" href="http://kepler-452b.net" target="_blank">Kepler-452b</a>의 Text 버전 비공식 스핀오프입니다.
+      </div>
 
-      <minting v-if="tab === 1" />
+      <div class="mb-3">
+        텍스트 케플러는 100% 온체인으로 구축되었습니다.<br />
+        클레이튼 네트워크가 존재하는 한 무한히 증식합니다.
+      </div>
+
+      <div class="mb-3">
+        성장주기: 세포 -> 새싹 -> 성체 -> 산란 -> 새싹<br />
+        *산란 후 레벨이 오르며, 새로운 세포가 생깁니다.
+      </div>
+
+      <div class="mb-3">
+        채집을 통해 우주 씨앗을 얻을 수 있습니다.<br />
+        우주 씨앗으로 텍스트 케플러를 얻거나 진화시킬 수 있습니다.<br />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import NavHeader from '@/components/NavHeader.vue';
-import Intro from '@/components/Intro.vue';
-import Minting from '@/components/Minting.vue';
 
 export default {
   components: {
-    NavHeader,
-    Intro,
-    Minting
-  },
-
-  data() {
-    return {
-      tab: 0
-    }
+    NavHeader
   }
 }
 </script>
@@ -51,8 +57,8 @@ export default {
 }
 
 .container {
-  margin-top: 25vh;
-  margin-bottom: 25vh;
+  margin-top: 20vh;
+  margin-bottom: 20vh;
   margin-left: 1rem;
   margin-right: 1rem;
   border: 0.3rem solid white;
