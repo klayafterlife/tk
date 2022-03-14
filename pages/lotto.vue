1966d2b93f7b4ec9677b8becbf39c8f556e1ba9e
klayafterlife <klayafterlife@gmail.com>
2022-03-14 22:47:37 +0900

    설명 추가

--- pages/lotto.vue
+++ pages/lotto.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="container">
-    <h1 class="mb-5 mt-5 pb-5">Ketto</h1>
+    <h1 class="mb-5 mt-5">Ketto</h1>
 
     <div class="mb-3">
       케또는 <a class="link" href="https://twitter.com/UppxS" target="_blank">축복님</a>의 아이디어로 탄생했습니다.<br />
       2 KLAY, 10%의 확률, 메타세포 케플러!
     </div>
 
-    <div class="mb-5">
+    <div class="mb-3">
       10%의 확률로 세포 상태의 진짜 케플러에 당첨됩니다.<br />
-      90% 확률로 두 개의 씨앗이 채집됩니다.<br />
+      90% 확률로 두 개의 씨앗이 채집됩니다.
+    </div>
+
+    <div class="mb-5">
+      수익의 90%는 추첨을 위한 케플러 확보에 사용됩니다.<br />
+      케또는 재미있게 메타세포를 얻을 수 있는 소소한 즐거움과 동시에 FP 반등을 위한 케플러 락업을 추구합니다.
     </div>
 
     <ConnectWallet v-if="!connected" />
